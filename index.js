5bf6e169f9f7363df650b43e6dfa5569c82f126a
anzalomar <oanisa143@gmail.com>
2024-06-21 23:14:48 +0300

    complet lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// Function to determine greeting based on the number of feet
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  } else {
+    return 'That will be twenty bucks.'; // Assuming this is for distances between 401 and 2000 feet
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// Function to check city using ternary operator
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// Function to return message based on the tip using switch statement
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
